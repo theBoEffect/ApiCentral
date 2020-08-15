40c52f7e59ace85c917e3d419335a94d9dc04ec2
Bo Motlagh <borzou@theboeffect.com>
2020-08-15 11:51:44 -0400

    fixing file ref

--- addSettingsToPortal.js
+++ addSettingsToPortal.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
-const bSettings = fs.existsSync('../settings.json');
-const siteSettings = (bSettings) ? require('../settings.json') : require('../settings-default.json');
+const bSettings = fs.existsSync('./settings.json');
+const siteSettings = (bSettings) ? require('./settings.json') : require('./settings-default.json');
 
 (async function(){
     if(bSettings === false) {
-        fs.writeFileSync('../settings.json', JSON.stringify(siteSettings, null, 2));
+        fs.writeFileSync('./settings.json', JSON.stringify(siteSettings, null, 2));
     }
     if(fs.existsSync('./portal')){
         fs.writeFileSync('./portal/settings.json', JSON.stringify(siteSettings, null, 2));
