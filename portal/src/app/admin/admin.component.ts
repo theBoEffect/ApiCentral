58cf0f4f4a9c8c96be385c6c6dfd893c4114297c
Bo Motlagh <borzou@theboeffect.com>
2020-08-18 20:38:01 -0400

    data wired, just need service calls

--- portal/src/app/admin/admin.component.ts
+++ portal/src/app/admin/admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {environment} from "../../environments/environment";
 import {SchemasService} from "../providers/schemas.service";
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -10,38 +11,76 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 })
 export class AdminComponent implements OnInit {
   public apis: any;
-  private closeResult: string;
+  public addForm: FormGroup;
   constructor(
     public SchService: SchemasService,
-    private modalService: NgbModal
-  ) { }
+    private formBuilder: FormBuilder,
+    private modalService: NgbModal,
+  ) {
+    this.addForm = this.formBuilder.group({
+      apiSpecJsonUri: ['', Validators.required],
+      swaggerUiUrl: [''],
+      displayTitle: ['',  Validators.required],
+      displayDescription: ['', Validators.required],
+    });
+  }
   public settings:any = environment.setting;
   async refresh(){
     try {
       const resp = await this.SchService.getApis().toPromise();
       this.apis = resp.data;
+      console.info(this.apis)
     } catch (error) {
       console.error(error);
     }
   }
 
-  open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
+  async open(content, api:any = undefined) {
+    try {
+      let edit = false;
+      this.addForm.reset();
+      if(api !== undefined) {
+        edit = true;
+        this.addForm.patchValue({apiSpecJsonUri : api.apiSpecJsonUri });
+        this.addForm.patchValue({swaggerUiUrl: api.swaggerUiUrl });
+        this.addForm.patchValue({displayTitle: api.displayTitle });
+        this.addForm.patchValue({displayDescription: api.displayDescription });
+      }
+      const result = await this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result;
+      if (edit) {
+        console.info('UPDATING');
+        return this.updateApi();
+      }
+      console.info( `WE HAVE ${result}`);
+      return this.saveNew();
+    } catch (error) {
+      console.info( `error ${error}`)
     }
   }
+
+  async updateApi() {
+    console.info('this worked update');
+    console.info(this.addForm.getRawValue());
+    //todo call service and update api record
+    this.addForm.reset();
+
+  }
+
+  async saveNew() {
+    console.info('this worked new');
+    console.info(this.addForm.getRawValue());
+    //todo call service and make api record
+    this.addForm.reset();
+  }
+
+  async delete(id) {
+    try {
+      console.info(`DELETE ${id}`);
+    } catch (error) {
+      //show
+    }
+  }
+
   ngOnInit(): void {
     this.refresh();
   }
--- portal/src/app/app.module.ts
+++ portal/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -26,7 +26,8 @@ import { SchemasService} from "./providers/schemas.service";
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [
     SchemasService
